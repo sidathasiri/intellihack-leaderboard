c65352f413498784a34cab66564c4f2cda14db28
Sidath <sidath.asiri@syscolabs.com>
2019-03-26 16:50:09 +0530

    implemented basic features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import data from "./data.json";
+
+import ListItem from "./Components/ListItem/ListItem";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        { name: "one", value: 12.75 },
+        { name: "two", value: 11.76 },
+        { name: "three", value: 11.76 },
+        { name: "four", value: 11.76 }
+      ]
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <img src="/syscoLABS.png" style={{ width: "20%", marginLeft: "78%" }} />
+        <div className="container" style={{ marginTop: 50 }}>
+          <img
+            src="/intellihack.png"
+            style={{ marginBottom: 20, marginLeft: 100 }}
+          />
+          <h1 style={{ marginLeft: 200, marginBottom: 30 }}>
+            Dengue Disease Prediction Challenge
+          </h1>
+          <h2>Leaderboard</h2>
+          <div className="list-group">
+            {data.map((item, index) => {
+              return <ListItem rank={index} data={item} key={index} />;
+            })}
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Components/ListItem/ListItem.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+export default class ListItem extends Component {
+  getTrophies(i) {
+    if (i == 1) {
+      return (
+        <span>
+          <img src="/gold.png" style={{ height: 70 }} />
+        </span>
+      );
+    } else if (i == 2) {
+      return (
+        <span>
+          <img src="/silver.png" style={{ height: 70 }} />
+        </span>
+      );
+    } else if (i == 3) {
+      return (
+        <span>
+          <img src="/bronze.png" style={{ height: 70 }} />
+        </span>
+      );
+    } else {
+      return;
+    }
+  }
+
+  render() {
+    const { name, value } = this.props.data;
+    const rank = this.props.rank + 1;
+    return (
+      <div style={{ margin: 5 }}>
+        <a
+          href="#"
+          className="list-group-item list-group-item-action disabled"
+          tabIndex="-1"
+          aria-disabled="true"
+          style={{ height: 100 }}
+        >
+          <div className="row">
+            <div className="col-md-10">
+              {this.getTrophies(rank)}
+              {name}
+            </div>
+            <div className="col-md-2">
+              <span
+                className="badge badge-secondary"
+                style={{ marginTop: 25, marginLeft: "80%", fontWeight: "bold" }}
+              >
+                {value}
+              </span>
+            </div>
+          </div>
+        </a>
+      </div>
+    );
+  }
+}
